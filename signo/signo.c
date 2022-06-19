c7afa33e47a78f56e39a631c1388df665b533a81
AllanLima <plimaallan@gmail.com>
2022-06-19 20:27:41 -0300

    Caracteres acentuados em hex

--- signo/signo.c
+++ signo/signo.c
@@ -1,72 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
 
 int main(){
 
-    int dia, mes, tentar;
-    
+    int dia, mes;
+    char tentar;
+
     do{
 
-        printf("\n\n\n\n\t\tDescubra o seu signo dos cavaleiros do zodíacos");
+        printf("\n\n\n\n\t\t\tDescubra o seu Signo do Zod\xA1\aaco");
 
         printf("\n\n\n\t\t\tInforme seu dia de nascimento: ");
         scanf("%d", &dia);
 
-        printf("\n\t\t\tInforme seu mês de nascimento: ");
+        printf("\n\t\t\tInforme seu m\x88s de nascimento: ");
         scanf("%d", &mes);
 
         switch (mes) {
 
             case 12:
-                printf("\n\t\t\tSeu signo é: %s", (dia >= 22) ? "Capricórnio" : "Sagitário");
+                printf("\n\t\t\tSeu signo \x82: %s", (dia >= 22) ? "Capric\xa2rnio" : "Sagit\xa0rio");
                 break;
             case 11:
-                printf("\n\t\t\tSeu signo é: %s", (dia >= 23) ? "Sagitário" : "Escorpião");
+                printf("\n\t\t\tSeu signo \x82: %s", (dia >= 23) ? "Sagit\xa0rio" : "Escorpi\xc6o");
                 break;
             case 10:
-                printf("\n\t\t\tSeu signo é: %s", (dia >= 23) ? "Escorpião" : "Libra");
+                printf("\n\t\t\tSeu signo \x82: %s", (dia >= 23) ? "Escorpi\xc6o" : "Libra");
                 break;
             case 9:
-                printf("\n\t\t\tSeu signo é: %s",  (dia >= 23) ? "Libra" : "Virgem");
+                printf("\n\t\t\tSeu signo \x82: %s",  (dia >= 23) ? "Libra" : "Virgem");
                 break;
             case 8:
-                printf("\n\t\t\tSeu signo é: %s",  (dia >= 23) ? "Virgem" : "Leão");
+                printf("\n\t\t\tSeu signo \x82: %s",  (dia >= 23) ? "Virgem" : "Le\xc6o");
                 break;
             case 7:
-                printf("\n\t\t\tSeu signo é: %s",  (dia >= 23) ? "Leão" : "Câncer");
+                printf("\n\t\t\tSeu signo \x82: %s",  (dia >= 23) ? "Le\xc6o" : "Câncer");
                 break;
             case 6:
-                printf("\n\t\t\tSeu signo é: %s",  (dia >= 22) ? "Câncer" : "Gêmeos");
+                printf("\n\t\t\tSeu signo \x82: %s",  (dia >= 22) ? "Câncer" : "G\x88meos");
                 break;
             case 5:
-                printf("\n\t\t\tSeu signo é: %s",  (dia >= 22) ? "Gêmeos" : "Tauro");
+                printf("\n\t\t\tSeu signo \x82: %s",  (dia >= 22) ? "G\x88meos" : "Tauro");
                 break;
             case 4:
-                printf("\n\t\t\tSeu signo é: %s",  (dia >= 21) ? "Tauro" : "Áries");
+                printf("\n\t\t\tSeu signo \x82: %s",  (dia >= 21) ? "Tauro" : "\xb5ries");
                 break;
             case 3:
-                printf("\n\t\t\tSeu signo é: %s",  (dia >= 21) ? "Áries" : "Peixes");
+                printf("\n\t\t\tSeu signo \x82: %s",  (dia >= 21) ? "\xb5ries" : "Peixes");
                 break;
             case 2:
-                printf("\n\t\t\tSeu signo é: %s",  (dia >= 20) ? "Peixes" : "Aquário");
+                printf("\n\t\t\tSeu signo \x82: %s",  (dia >= 20) ? "Peixes" : "Aqu\xa0rio");
                 break;
             case 1:
-                printf("\n\t\t\tSeu signo é: %s",  (dia >= 21) ? "Aquário" : "Capricórnio");
+                printf("\n\t\t\tSeu signo \x82: %s",  (dia >= 21) ? "Aqu\xa0rio" : "Capric\xa2rnio");
                 break;
             default:
-                printf("\n\t\t\t\tValor inválido!!!");
+                printf("\n\t\t\t\tValor inv\xa0lido!!!");
                 break;
 
         }
 
-        printf("\n\n\t\tAperte 1 para tentar novamente ou 2 para sair: ");
-        scanf("%d", &tentar);
+        printf("\n\n\t\tAperte 'S' e descobra novamente ou outra tecla para sair \n");
+        tentar = getch();
 
-        if (tentar == 1) {
+        if (tentar == 'S' || tentar == 's') {
             system("cls");
         }
 
-    } while(tentar == 1);
+    } while (tentar == 'S' || tentar == 's');
 
     return 0;
 
