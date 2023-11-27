f91354957e741ccb4d660b63909372024234aadc
Allan <plallan@gmail.com>
2023-11-26 22:11:53 -0300

    Atualizar  maior_valor_matriz

--- ArrayVetorMatriz/maior_valor_matriz.c
+++ ArrayVetorMatriz/maior_valor_matriz.c
@@ -2,17 +2,34 @@
 #include <stdlib.h>
 
 int main() {
-  int lista[3][3] = {{10, 15, 13}, {12, 14, 17}, {16, 19, 18}};
-  int maior_valor, maior_valor_retido, valor_index = 0;
+
+  // Matriz lista de valores
+  int lista[3][3] = {{10, 15, 13}, 
+                     {12, 14, 17}, 
+                     {16, 19, 18}};
+                     
+  // Iniciar variaveis 'maior_valor', 'valor_retido' e 'valor_index'
+  int maior_valor, valor_retido, valor_index = 0;
+
+  // Percorrer valores das linhas da matrix
   for (int linha = 0; linha < 3; linha++) {
+
+    // Percorrer valores das coluna da matrix
     for (int coluna = 0; coluna < 3; coluna++) {
       valor_index = lista[linha][coluna];
-      maior_valor_retido = maior_valor;
+      valor_retido = maior_valor;
+
+      // Se valor do index for maior que 'maior_valor', fazar a troca
       if (maior_valor < lista[linha][coluna]) {
         maior_valor = lista[linha][coluna];
       }
-      printf("Maior Valor Retido: %d <> Valor index: %d = Maior Valor: %d \n", maior_valor_retido, valor_index, maior_valor);
-    }
-  }
+
+      // Imprimir valor retido, valor index e maior valor
+      printf("Valor Retido: %d <> Valor index: %d = Maior Valor: %d \n", valor_retido, valor_index, maior_valor);
+
+    } // Fim for coluna
+  } // Fim for Linha
+
   return 0;
+
 }
