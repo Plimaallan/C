16034ce6048a0938790353a5f9e1eaa38c2d283b
Allan <plallan@gmail.com>
2023-12-01 22:08:10 -0300

    atualização do código

--- strings/inverter_string.c
+++ strings/inverter_string.c
@@ -5,24 +5,35 @@
 int main() {
 
   // Cria dois arrays para 11 caracteres e '\0'
-  char nome[11] = "Allan Lima";
-  char nome_invertido[11];
+  char nome[20] = "Allan Lima";
+  char nome_invertido[20];
 
   // Recebe o numero de caractere da variavel 'nome'
-  char nome_tamanho = strlen(nome) - 1;
+  char nome_tamanho = strlen(nome);
 
   // Inverter usando for e imprimindo caractere por caractere
-  for (int i = nome_tamanho; i >= 0; i--) {
+  for (int i = nome_tamanho - 1; i >= 0; i--) {
     printf("%c", nome[i]);
   }
 
   // Inverter usando for para transferir os caracteres de 'nome' para 'nome_invertido'
   for (int i = nome_tamanho; i >= 0; i--) {
-    nome_invertido[i] = nome[nome_tamanho - i];
+    nome_invertido[i] = nome[nome_tamanho - 1 - i];
   }
 
   // Imprimir o array 'nome_invertido'
   printf("\nNome invertido: %s\n", nome_invertido);
+
+  // Inverter usando for para transferir os caracteres de 'nome' para 'valor_index' e 'nome'
+  char letra_temporaria;
+  for (int i = 0; i < nome_tamanho / 1; i++) {
+    letra_temporaria = nome[i];
+    nome[i] = nome[nome_tamanho - 1 - i];
+    nome[nome_tamanho - 1 - i] = letra_temporaria;
+  }
+
+  // Imprimir o array 'nome'
+  printf("Nome corrigido: %s\n", nome);
 
   return 0;
 
