e479363292b124c13544c6c996476ccb7d492120
Allan <plallan@gmail.com>
2023-12-12 21:39:06 -0300

    otimização do código

--- ArrayVetorMatriz/soma.c
+++ ArrayVetorMatriz/soma.c
@@ -2,10 +2,19 @@
 #include <stdlib.h>
 
 int main() {
-  int lista[10] = {10, 15, 12, 12, 14, 15, 16, 19, 18, 20};
+
+  // Array com 10 valores definidos
+  int lista[10] = { 10, 15, 12, 12, 14, 15, 16, 19, 18, 20 };
+
+  // Variável para receber a soma de todos os valores
   int soma = 0;
+
+  // For percorre e imprime todos os valores da soma
   for (int i = 0; i < 10; i++) {
     soma = soma + lista[i];
     printf("Total %d: %d \n", i, soma);
-  }
+  } // Fim for soma
+
+  return 0;
+  
 }
