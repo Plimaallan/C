ed67230ca9b8b8b56fd3612f9be75f505178ff65
Allan <plallan@gmail.com>
2023-12-26 13:30:49 -0300

    passagem parametro

---
+++ passagem_parametro/somar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int somar_dois_numeros(int x, int y) {
+
+  int resultado = x * y;
+  printf("O produto de %d multriplicado por %d é: %d \n", x, y, resultado);
+
+  return 0;
+
+}
+
+int main() {
+
+  somar_dois_numeros(10, 20);
+  somar_dois_numeros(20, 30);
+  somar_dois_numeros(50, 2);
+
+  return 0;
+
+}
